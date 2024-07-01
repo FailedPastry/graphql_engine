fc1c6a77e079d2d2d1eae2f279b6805ea21fe0e3
FailedPastry <mario.tangmo@gmail.com>
2024-07-01 08:43:36 -0500

    completed model 21 challenge

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
-# graphql_engine
- 
+# GraphQL Engine
+
+# Description
+With this GraphQL app utilizing the Google Books API, search for any books you like and save them to your personal login.
+
+# Usage
+## Website Preview
+- Website Link: https://graphql-engine-5zyl.onrender.com
+
+![Image of the deployed website.](./client/src/assets/Capture.PNG)
+# Tools
+- [Apollo](https://www.apollographql.com/docs/apollo-server/)
+- [Bootstrap](https://getbootstrap.com/)
+- [GraphQL](https://graphql.org/)
+- [React](https://react.dev/)
+- [Express](https://expressjs.com/)
+- [JWT](https://jwt.io/)
+- [Mongoose](https://mongoosejs.com/)
---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,48 @@
+const typeDefs = `
+
+    type Query {
+        me: [User]
+    }
+
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
+
+    type Book {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+
+    input userBooks {
+        bookId: ID
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
+        
+    type Auth {
+        token: ID
+        user: User
+    }
+
+    type Mutation {
+        login(email: String, password: String): Auth
+        addUser(username: String, email: String, password: String): Auth
+        saveBook(bookdata: userBooks): User
+        removeBook(bookId: ID): User
+    }
+
+`;
+
+
+module.exports = typeDefs;
